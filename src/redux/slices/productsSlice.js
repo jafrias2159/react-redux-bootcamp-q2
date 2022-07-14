72f8dec7dfcb6a79da371e9fd29160e076bd638d
Jorge Frias <Jorge_Frias@epam.com>
2022-07-14 15:06:48 -0500

    Adding produc reducer

---
+++ src/redux/slices/productsSlice.js
@@ -0,0 +1,43 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { convertArrayToObjectByKey } from '../../utils/convertArray';
+
+const fetchAllProducts = createAsyncThunk(
+  'products/fetchAllProducts',
+  async () => {
+    const requestResponse = await fetch(
+      'https://6x8prpit9f.execute-api.us-east-1.amazonaws.com/api/products',
+      {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          'x-api-key': '94tOOmgSAz3V6Xe7f2cMuz6qjgrIXS2xq8Xbly00',
+        },
+      }
+    );
+
+    const { items } = await requestResponse.json();
+    return items;
+  }
+);
+
+const initialState = {
+  products: [],
+  searchProducts: [],
+};
+
+const productsSlice = createSlice({
+  name: 'products',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchAllProducts.fulfilled, (state, action) => {
+      const items = convertArrayToObjectByKey(action.payload, 'id');
+      state.products = action.payload;
+      state.searchProducts = items;
+    });
+  },
+});
+
+export const allProductsSelector = (state) => state.products;
+export { fetchAllProducts };
+export default productsSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import productsSlice from "./slices/productsSlice";
 import userSlice from "./slices/userSlice";
 
 const store = configureStore({
   reducer: {
     user: userSlice,
+    products: productsSlice
   },
 });
 
