4b99500e2d1ae766f0956584a36fcd7029da37e9
Jorge Frias <Jorge_Frias@epam.com>
2022-07-15 14:17:04 -0500

    Remving useref

--- src/components/OrderDetails/index.js
+++ src/components/OrderDetails/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Grid } from '../../components/ProductsGrid/ProductsGrid.style';
 import Product from '../Products';
